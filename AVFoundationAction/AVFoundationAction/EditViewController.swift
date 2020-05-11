060b99a6d8bc493453a81b53067723e874170140
Ezio <cgjacken@gmail.com>
2020-05-11 20:16:19 +0800

    音视频混合剪切以及导出

---
+++ AVFoundationAction/AVFoundationAction/EditViewController.swift
@@ -0,0 +1,113 @@
+//
+//  EditViewController.swift
+//  AVFoundationAction
+//
+//  Created by EzioChan on 2020/5/7.
+//  Copyright © 2020 Ezio. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class EditViewController: UIViewController {
+    
+    @IBOutlet weak var playerImgv: UIImageView!
+    var composotion:AVMutableComposition!
+    var firstAsset:AVAsset!
+    var secAsset:AVAsset!
+    var path:String!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        path = Bundle.main.path(forResource: "IMG_2735", ofType: "mp4")
+        let url = URL.init(fileURLWithPath: path)
+        let options = [AVURLAssetAllowsCellularAccessKey:false,AVURLAssetPreferPreciseDurationAndTimingKey:true]
+        firstAsset = AVURLAsset.init(url: url, options: options)
+        
+        let voicePath = Bundle.main.path(forResource: "SuperMarioBros", ofType: "mp3")
+        let url2 = URL.init(fileURLWithPath: voicePath!)
+        secAsset = AVURLAsset.init(url: url2)
+        
+        let voicePath2 = Bundle.main.path(forResource: "MaiDou", ofType: "mp3")
+        let url3 = URL.init(fileURLWithPath: voicePath2!)
+        let thireAsset = AVURLAsset.init(url: url3)
+        
+        let videoAssetTrack = firstAsset.tracks(withMediaType: .video).first
+        let audioAssetTrack = secAsset.tracks(withMediaType: .audio).first
+        let audioAssetTrack2 = thireAsset.tracks(withMediaType: .audio).first
+        
+        //合成器
+        composotion = AVMutableComposition.init()
+        //视频轨道
+        let videoCompostiontrack = composotion.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid)
+        //音频轨道1
+        let audioCompostiontrack = composotion.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid)
+        //音轨2
+        let audioCompositiontrack2 = composotion.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid)
+        
+        //为视频轨道放入视频
+        let v = 0.8 * Float(firstAsset.duration.seconds) * videoAssetTrack!.nominalFrameRate
+        let e = 0.1 * Float(firstAsset.duration.seconds) * videoAssetTrack!.nominalFrameRate
+        let v_start = CMTime.init(value: CMTimeValue(v), timescale: CMTimeScale(videoAssetTrack!.nominalFrameRate))
+        let v_endTime = CMTime.init(value: CMTimeValue(e), timescale: CMTimeScale(videoAssetTrack!.nominalFrameRate))
+        print(v_start.seconds)
+        print(v_endTime.seconds)
+        let theTime = CMTimeRangeMake(start: v_start, duration: v_endTime)
+        
+        try?videoCompostiontrack?.insertTimeRange(theTime, of: videoAssetTrack!, at: CMTime.zero)
+        //为音频轨道放入音频
+        let vex = CMTime.init(value: CMTimeValue(e), timescale: CMTimeScale(videoAssetTrack!.nominalFrameRate)).seconds - audioAssetTrack2!.timeRange.duration.seconds
+        let theTimeAudio = CMTimeRangeMake(start: CMTime.zero, duration: CMTime.init(value: CMTimeValue(6.48275), timescale: CMTimeScale(1.0)))
+        try?audioCompostiontrack?.insertTimeRange(theTimeAudio, of: audioAssetTrack!, at: CMTime.zero)
+        //为音频轨道放入音频2
+        let theTimeAudio2 = CMTimeRangeMake(start: CMTime.zero, duration: (audioAssetTrack2?.timeRange.duration)!)
+        let starTime2 = v_endTime.seconds - vex
+        let st2 = CMTime.init(value: CMTimeValue(6.48275), timescale: CMTimeScale(1.0))
+        print(st2.seconds)
+        try?audioCompositiontrack2?.insertTimeRange(theTimeAudio2, of: audioAssetTrack2!, at: st2)
+        
+        
+        
+    
+        let exporter = AVAssetExportSession.init(asset: composotion, presetName: AVAssetExportPreset1280x720)
+        let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first
+        let filePath = path!+"/test.mp4"
+        try?FileManager.default.removeItem(atPath: filePath)
+//        FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil)
+        
+        exporter?.outputURL = URL.init(fileURLWithPath: filePath)
+        
+        exporter?.outputFileType = .mp4
+        exporter?.shouldOptimizeForNetworkUse = true
+        exporter?.exportAsynchronously(completionHandler: {
+            if exporter?.error != nil{
+                print(exporter?.error)
+            }else{
+            
+            }
+            DispatchQueue.main.async {
+                self.playIt(url: URL.init(fileURLWithPath: filePath))
+            }
+        })
+        
+        
+        
+        
+    }
+    
+    func playIt(url:URL) {
+        let item = AVPlayerItem.init(url: url)
+        let player = AVPlayer.init(playerItem: item)
+        let layer = AVPlayerLayer.init(player: player)
+        layer.frame = playerImgv.frame
+        layer.videoGravity = .resizeAspect
+        playerImgv.layer.addSublayer(layer)
+
+        player.play()
+
+    }
+
+    func createcomposition()  {
+        
+    }
+
+}
--- AVFoundationAction/AVFoundationAction/ViewController.swift
+++ AVFoundationAction/AVFoundationAction/ViewController.swift
@@ -9,21 +9,136 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource{
+
+    
     var path:String!
     var mainAsset:AVAsset!
+    var imageGen:AVAssetImageGenerator!
+    @IBOutlet weak var imagesTable: UITableView!
+    var imagesArray:[UIImage]! = []
+    @IBOutlet weak var slider: UISlider!
+    var tmpValue:Float!
+    @IBOutlet weak var thumbImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        path = Bundle.main.path(forResource: "IMG_0830", ofType: "MOV")
+        
+        imagesTable.delegate = self
+        imagesTable.dataSource = self
+        imagesTable.rowHeight = 72
+        imagesTable.register(UITableViewCell.self, forCellReuseIdentifier: "testCell")
+        
+        path = Bundle.main.path(forResource: "IMG_2735", ofType: "mp4")
         let url = URL.init(fileURLWithPath: path)
         let options = [AVURLAssetAllowsCellularAccessKey:false,AVURLAssetPreferPreciseDurationAndTimingKey:true]
         mainAsset = AVURLAsset.init(url: url, options: options)
+        basicData()
+        track()
+        getAllFrame()
+        initUI()
+        
+    }
     
+    func initUI() {
+        slider.maximumValue = 1.0;
+        slider.minimumValue = 0.0;
+        slider.setValue(0.0, animated: true)
+        slider.addTarget(self, action: #selector(changeAction), for: .valueChanged)
+        imageGen = AVAssetImageGenerator.init(asset: mainAsset)
+        imageGen.maximumSize = CGSize.init(width: 1170, height: 540)
+        imageGen.appliesPreferredTrackTransform = true
+    }
+    
+    @objc func changeAction(){
+        print("slider.value:\(slider.value)")
+       
+        let imgc = mainAsset.tracks(withMediaType: .video).count
+        var track:AVAssetTrack!
+        if imgc == 1 {
+            track = mainAsset.tracks(withMediaType: .video).first
+        }
+        let v = slider.value * Float(mainAsset.duration.seconds) * track.nominalFrameRate
+        let t = CMTime.init(value: CMTimeValue(v), timescale: CMTimeScale(track.nominalFrameRate))
+        imageGen.cancelAllCGImageGeneration()
+        guard let img = try?self.imageGen.copyCGImage(at: t, actualTime: nil) else {
+            return
+        }
+        print("get a img by asset")
+        self.thumbImageView.image = UIImage.init(cgImage: img)
+//        DispatchQueue.global().async {
+//            guard let img = try?self.imageGen.copyCGImage(at: t, actualTime: nil) else {
+//                return
+//            }
+//            DispatchQueue.main.async {
+//                print("get a img by asset")
+//                self.thumbImageView.image = UIImage.init(cgImage: img)
+//            }
+//
+//        }
+    }
+    
+    func getAssetAttrabute(){
+        
+        let mateData = mainAsset.metadata(forFormat: .quickTimeUserData)
+        print("元数据标记数组\(mateData)")
+        
+    }
+    
+    func getAllFrame(){
+        
+        let imgc = mainAsset.tracks(withMediaType: .video).count
+        var track:AVAssetTrack!
+        if imgc == 1 {
+            track = mainAsset.tracks(withMediaType: .video).first
+        }
+        let gem = AVAssetImageGenerator.init(asset: mainAsset)
+        gem.maximumSize = CGSize.init(width: 128, height: 72)
+        gem.apertureMode = AVAssetImageGenerator.ApertureMode.cleanAperture
+        var array:[NSValue]! = []
+        let d = mainAsset.duration.seconds
+        let all = d * Double(track.nominalFrameRate)
+        
+        let numberGet = Int(d*5)//每秒里面取5帧
+        let scan = Int(all)/numberGet //取帧间隔
+        for item in 0..<numberGet {
+            let value = item*scan
+            let ctvalue = CMTime.init(value: CMTimeValue(value), timescale: CMTimeScale(track!.nominalFrameRate)) as NSValue
+            array.append(ctvalue)
+        }
+        
+        gem.generateCGImagesAsynchronously(forTimes: array) { (time1, cgimg, time2, result, error) in
+            if let _cgimg = cgimg {
+                let img = UIImage.init(cgImage: _cgimg)
+                self.imagesArray.append(img)
+                DispatchQueue.main.async {
+                    self.imagesTable.reloadData()
+                }
+                //print("get a img by asset\(img)")
+            }
+        }
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imagesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "testCell")!
+        cell.imageView?.image = imagesArray[indexPath.row]
+        return cell
+    }
+    
+    
+    func basicData() {
         
         let d = CMTimeGetSeconds(mainAsset.duration)
         let p = mainAsset.providesPreciseDurationAndTiming
-        print("时长：\(d)，可用时间准确性：\(p)")
+        print("时长：\(d)，可用时间准确性：\(p) \(mainAsset.duration.timescale)")
         
         let formatter = DateFormatter.init()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
@@ -57,19 +172,24 @@ class ViewController: UIViewController {
         let compatible = mainAsset.isCompatibleWithSavedPhotosAlbum
         print("是否可以写入相册：\(compatible)")
         
-        track()
-      
+        
     }
     
+    /// 查看所有轨道的数据信息
     func track() {
+        
         for track in mainAsset.tracks {
             print("trackID:\(track.trackID)")
             print("mediaType:\(track.mediaType.rawValue)")
+            print("nominalFrameRate:\(track.nominalFrameRate)")
+            print("minFrameDuration:\(track.minFrameDuration.seconds)")
+            print("formatDescription:\(track.formatDescriptions)\n\n\n\n\n\\n\n\n\n\n\n")
             
-            //print("formatDescription:\(track.formatDescriptions)")
         }
     }
-
-
+    
+    
+    
+    
 }
 
